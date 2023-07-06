8fb476b8d60f36d082dc59264ad5bba8ceab8ed5
masima1972 <h_suya@hotmail.com>
2023-07-06 23:14:10 +0900

    辞書型の初期化をテストする

--- Assets/MiniScript/Tests/MiniScriptTests_Dictionary.cs
+++ Assets/MiniScript/Tests/MiniScriptTests_Dictionary.cs
@@ -45,6 +45,35 @@ namespace MiniScript.Tests
 			}
 		}
 
+		/// <summary>
+		/// 辞書型変数参照
+		/// </summary>
+		/// <param name="index"></param>
+		[Test]
+		public void TestDictionary_Initialize(
+			[Values(0, 1, 2)] float a
+			, [Values(0, 1, 2)] float b
+			)
+		{
+			// 変数保存場所生成
+			var context = new Context();
+			context
+				.Set(nameof(a), a)
+				.Set(nameof(b), b)
+				;
+			{
+				var patterns = new (string sentence, float result)[]
+				{
+					// 空辞書を代入することで実行時のGC Allocを軽減する
+					("d=[];d.a=a;d.b=b;d.a+d.b", a+b),
+					// 初期値を指定する
+					("d=[a:a,b:b]; d.a+d.b", a+b),
+					("d=[a:a+b,b:a*b]; d.a+d.b", a+b+a*b),
+				};
+				TestPatterns(patterns, context);
+			}
+		}
+
 		[Test]
 		public void TestDictionary_GetByPath()
 		{
